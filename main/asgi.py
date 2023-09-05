d4ab4e586cb1ea4f829d877a8293439b62ac3795
Samson Siba <69885978+purpleapples@users.noreply.github.com>
2023-09-05 09:26:21 +0900

    install package django-redis (suspend until configuring redis-server)

--- main/asgi.py
+++ main/asgi.py
@@ -11,16 +11,15 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+import system.routing
 import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings.prod')
 
 application = ProtocolTypeRouter({
 	"http": get_asgi_application(),
-	# "websocket":AllowedHostsOriginValidator(
-	# 	AuthMiddlewareStack(
-	# 		URLRouter()
-	# 	)
-	# )
+	"websocket":AllowedHostsOriginValidator(
+		AuthMiddlewareStack(URLRouter(system.routing.websocket_urlpatterns))
+	),
 })
 
