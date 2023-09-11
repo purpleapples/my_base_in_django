73523216bbd9300f2ba9684f588e9a68e8f43673
Samson Siba <69885978+purpleapples@users.noreply.github.com>
2023-09-11 13:50:10 +0900

    Update template.py

--- base/views/code_table/template.py
+++ base/views/code_table/template.py
@@ -9,8 +9,8 @@ class CodeTableListView(ConditionalListView):
 
     def get_queryset(self):
         qs = super().get_queryset()
-        if 'parent_id' in self.kwargs.keys():
-            qs = qs.filter(**self.kwargs)
+        if 'parent_code' in self.kwargs.keys():
+            qs = qs.filter(parent__code=self.kwargs['parent_code'])
         else:
             qs = qs.filter(level=0)
         return qs.order_by(*self.order_by)
@@ -18,8 +18,8 @@ class CodeTableListView(ConditionalListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data()
 
-        if 'parent_id' in self.kwargs.keys():
-            parent = self.model.objects.get(id=self.kwargs['parent_id'])
+        if 'parent_code' in self.kwargs.keys():
+            parent = self.model.objects.get(code=self.kwargs['parent_code'])
             context['parent'] = parent
             context['title'] = parent.name + ' 하위 '
             context['category'] = parent.category
